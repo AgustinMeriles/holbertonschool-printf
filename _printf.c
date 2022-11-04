8eeeda59df11b3f550656e26a3e144b49b2078bc
AgustinMeriles <5720@holbertonstudents.com>
2022-11-04 06:49:12 -0700

    Creation of main h, and the function

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * printf - function that produces output according to a formaint.
+ * @format: is a character string.
+ * Return: the number of characters printed.
+ **/
+int _printf(const char *format, ...)
+{
+
+}
