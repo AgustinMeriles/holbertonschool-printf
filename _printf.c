b1dc0ca37746a8e64c767876b078513556e6ca56
AgustinMeriles <5720@holbertonstudents.com>
2022-11-07 12:41:27 -0800

    continuation of the thinking of printf

--- _printf.c
+++ _printf.c
@@ -6,5 +6,23 @@
  **/
 int _printf(const char *format, ...)
 {
+	int cont, a = 0;
+	const char letter;
+	va_list lis;
+	
+	va_start(lis, format);
+	while (!format)
+	{
+		return (-1);
+	}
+	while (format[a] == "%")
+	{
+		a++;
+		if (format[a] == "c")
+		{
+
+		}
+		if (format[a] == "s")
+		{
 
 }
