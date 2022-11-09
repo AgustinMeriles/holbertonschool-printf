85bf98d7f8ec6c49ec30dde679d0a88eb6e5e6e3
AgustinMeriles <5720@holbertonstudents.com>
2022-11-08 17:21:54 -0800

    _printf function

--- _printf.c
+++ _printf.c
@@ -1,28 +1,44 @@
 #include "main.h"
 /**
- * printf - function that produces output according to a formaint.
+ * _printf - function that produces output according to a formaint.
  * @format: is a character string.
  * Return: the number of characters printed.
  **/
 int _printf(const char *format, ...)
 {
-	int cont, a = 0;
-	const char letter;
-	va_list lis;
-	
-	va_start(lis, format);
-	while (!format)
-	{
+	int i = 0, counter = 0;
+	va_list arg;
+	int (*f)(va_list);
+
+	if (format == NULL)
 		return (-1);
-	}
-	while (format[a] == "%")
+	va_start(arg, format);
+	while (format[i] != '\0')
 	{
-		a++;
-		if (format[a] == "c")
+		while (format[i] != '%' && format[i] != '\0')
 		{
-
+			_putchar(format[i]);
+			counter++;
+			i++;
 		}
-		if (format[a] == "s")
+		if (format[i] == '\0')
+			return (counter);
+		f = get_op_func(&format[i + 1]);
+		if (f != NULL)
 		{
-
+			counter += f(arg);
+			i += 2;
+			continue;
+		}
+		if (format[i + 1] == '\0')
+			return (-1);
+		_putchar(format[i]);
+		counter++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(arg);
+	return (counter);
 }
